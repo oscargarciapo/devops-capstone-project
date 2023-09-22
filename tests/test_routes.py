2ebb384d18227fce06708b75e68237632f57fece
Oscar Garcia Porras <98061745+oscargarciapo@users.noreply.github.com>
2023-09-22 01:08:12 -0600

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -25,6 +25,8 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -180,15 +182,16 @@ class TestAccountService(TestCase):
     #     response = self.client.get("/", environ_overrides=HTTPS_ENVIRON)
     #     self.assertEqual(response.status_code, status.HTTP_200_OK)
     #     headers = {'X-Frame-Options': 'SAMEORIGIN', 'X-XSS-Protection': "1; mode=block",
-    #                'X-Content-Type-Options': 'nosniff', 
+    #                'X-Content-Type-Options': 'nosniff',
     #                'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
     #                'Referrer-Policy': 'strict-origin-when-cross-origin'}
     #     for key, value in headers.items():
     #         self.assertEqual(response.headers.get(key), value)
-    
+
     # def test_cors_security(self):
     #     """It should return a CORS header"""
     #     response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
     #     self.assertEqual(response.status_code, status.HTTP_200_OK)
     #     # Check for the CORS header
     #     self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
+  
